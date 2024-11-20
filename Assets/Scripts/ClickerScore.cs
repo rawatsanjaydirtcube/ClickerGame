e3bf3814d743a4c050210f7ae00c67ca83ba6462
rawatsanjaydirtcube <65915348+rawatsanjaydirtcube@users.noreply.github.com>
2024-11-20 12:42:30 +0530

    Updated script

--- Assets/Scripts/ClickerScore.cs
+++ Assets/Scripts/ClickerScore.cs
@@ -12,10 +12,38 @@ public class ClickerScore : MonoBehaviour
 
     public Text DebugArea;
 
+    public Dictionary<string, object> scoreDict = new();
+
+    private string name;
+
+    private void Start()
+    {
+        name = "User_" + Random.Range(0, 100);
+    }
+
     public void AddScore()
     {
         myScore++;
-
         ScoreText.text = myScore.ToString();
+        UpdateDict(name, myScore.ToString());
     }
+
+    public void UpdateDict(string username,string score)
+    {
+        scoreDict[username] = score;
+    }
+
+    public void SubmitScore()
+    {
+
+    }
+
+    public void DisplayScore()
+    {
+        foreach (var item in scoreDict)
+        {
+            DebugArea.text = $"Key is {item.Key} and value is {item.Value}";
+        }
+    }
+
 }
