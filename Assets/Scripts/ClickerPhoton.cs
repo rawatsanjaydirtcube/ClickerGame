317a21f04ab83d2080004041a7da50eebe0704fe
rawatsanjaydirtcube <65915348+rawatsanjaydirtcube@users.noreply.github.com>
2024-11-20 12:35:06 +0530

    Basic setup done for clicker game

--- Assets/Scripts/ClickerPhoton.cs
+++ Assets/Scripts/ClickerPhoton.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class ClickerPhoton : MonoBehaviour
+public class ClickerPhoton : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public TypedLobby testLobby = new TypedLobby("TestLobby", LobbyType.Default);
+
+    private void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowDebug(string message)
     {
-        
+        Debug.Log("<<<<" + message);
     }
+
+
+    //-----------------------------------------------------------------------------------------------
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        PhotonNetwork.JoinLobby(testLobby);
+        ShowDebug("OnConnectedToMaster");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        ShowDebug("On Joined Lobby");
+
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        ShowDebug("On Joined Room  " + PhotonNetwork.CurrentRoom.Name);
+    }
+
 }
